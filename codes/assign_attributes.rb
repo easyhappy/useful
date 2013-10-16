5ad729fe7ddad7776dba8322317a71e0da6fdb01
andyHu <meeasyhappy@gmail.com>
2013-10-16 22:56:48 +0800

    No message.

--- codes/assign_attributes.rb
+++ codes/assign_attributes.rb
@@ -1,4 +1,5 @@
 require 'active_record'
+require 'pry'
 ActiveRecord::Base.establish_connection(
   :adapter => "sqlite3",
   :database => ":memory"
@@ -6,12 +7,13 @@ ActiveRecord::Base.establish_connection(
 
 #ActiveRecord::Base.connection.execute("CREATE TABLE users (name String, is_admin boolean)")
 class User < ActiveRecord::Base
-  attr_accessible :name
-  attr_accessible :is_admin, :as => :admin
+  attr_accessible :name, :as => :admin
+  attr_accessible :name, :pis_admin, :as => :admin
 end
 
 user = User.new
 user.assign_attributes({ :name => 'Josh', :is_admin => true })
+binding.pry
 user.name       # => "Josh"
 user.is_admin?  # => false
 puts user.is_admin?
@@ -27,3 +29,7 @@ user.assign_attributes({ :name => 'Josh', :is_admin => true }, :without_protecti
 user.name       # => "Josh"
 user.is_admin?  # => true
 puts user.is_admin?
+
+user = User.new({:name => 'jsoin', :is_admin => true})
+puts user.name
+puts user.is_admin
